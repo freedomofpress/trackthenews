0adce8609405e2e8651863d3891a90ea14e6680b
Erik Moeller <erik@freedom.press>
2023-07-17 17:13:24 -0700

    Add flake8, black, isort & apply them; fix any issues

--- setup.py
+++ setup.py
@@ -1,33 +1,31 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     reqs = f.read().split()
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
 setup(
-    name='trackthenews',
-    version='0.2',
-    description='Monitor RSS feeds for keywords and act on matching results. A special project of the Freedom of the Press Foundation.',
+    name="trackthenews",
+    version="0.2",
+    description="Monitor RSS feeds for keywords and act on matching results."
+    " A special project of the Freedom of the Press Foundation.",
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     install_requires=reqs,
-    author='Parker Higgins',
-    author_email='parker@freedom.press',
-    url='https://github.com/freedomofpress/trackthenews',
-    entry_points={
-        'console_scripts': ['trackthenews=trackthenews:main']
-    },
-    package_data={
-        'trackthenews': ['fonts/*']
-    },
+    author="Parker Higgins",
+    author_email="parker@freedom.press",
+    url="https://github.com/freedomofpress/trackthenews",
+    entry_points={"console_scripts": ["trackthenews=trackthenews:main"]},
+    package_data={"trackthenews": ["fonts/*"]},
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9'],
-    packages=find_packages(exclude=('ttnconfig',))
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=find_packages(exclude=("ttnconfig",)),
 )
